fd6fb70230d97fdab0346e9136dcdbd284fb6a57
fatmakalia01-alt <fatmakalia01@gmail.com>
2026-02-23 06:06:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("MAIN VERSION: FIXED_REF_v5");
-createRoot(document.getElementById("root")!).render(<App />);
+console.log("MAIN VERSION: FIXED_REF_v6");
+createRoot(document.getElementById("root")!).render(React.createElement(App, null));
