20b2c536d39711681514b153b87fcd5f92df2a4f
fatmakalia01-alt <fatmakalia01@gmail.com>
2026-02-23 04:45:22 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -13,5 +13,7 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
+    // @ts-ignore
+    lock: { acquireTimeout: 15000 }
   }
 });
